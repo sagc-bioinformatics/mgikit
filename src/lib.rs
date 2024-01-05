b656b8e033b18a3b739de586a768a67765e9676b
ziadbkh <ziadbkh@gmail.com>
2024-01-05 17:35:19 +1100

    minor fix

--- src/lib.rs
+++ src/lib.rs
@@ -33,7 +33,7 @@ use crate::sequence_utils::*;
 mod index_dic;
 use crate::index_dic::*;
 
-const BUFFER_SIZE: usize = 1 << 17;
+const BUFFER_SIZE: usize = 1 << 20;
 const BUFFER_SIZE_MIN:usize = 1000000;
 //1048576
 
@@ -1645,7 +1645,7 @@ pub fn demultiplex(
             //println!("reading more!");
             //println!("{}---------------------", read_cntr);
             if read_end < read_bytes_2 - 1 && header_start > 0 {
-                copy_within_a_slice(&mut buffer_2, read_end + 1, 0, read_bytes_2 - read_end);
+                copy_within_a_slice(&mut buffer_2, read_end + 1, 0, read_bytes_2 - read_end - 1);
                 read_bytes_2 -= read_end + 1;
                 header_start = 0;
             }else if read_end == read_bytes_2 - 1{
